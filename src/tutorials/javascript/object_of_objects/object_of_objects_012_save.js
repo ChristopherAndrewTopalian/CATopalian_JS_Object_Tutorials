73970bf994831e66400202f736fc88a24c74ac50
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-07-11 03:26:38 -0400

    update

--- src/tutorials/javascript/object_of_objects/object_of_objects_012_save.js
+++ src/tutorials/javascript/object_of_objects/object_of_objects_012_save.js
@@ -1,4 +1,4 @@
-// object_of_objects_012.js
+// object_of_objects_012_save.js
 
 let worldObject = {
     "tree_01":
@@ -26,7 +26,6 @@ let worldObject = {
     }
 };
 
-
 // Keyboard Controls
 document.addEventListener('keydown', function(event)
 {
@@ -35,12 +34,45 @@ document.addEventListener('keydown', function(event)
     {
         let savedLevel = JSON.stringify(worldObject, null, 2);
 
-        console.log('World Saved!');
+        let date = new Date().toLocaleString('en-US');
+
+        console.log('World Saved!' + ' ' + date);
+
         console.log(savedLevel);
     }
 });
 
 //----//
+
+/*
+When we press the letter S,
+it will save the worldObject
+and also print it to console!
+*/
+
+/*
+World Saved! 7/11/2026, 3:25:01 AM
+{
+  "tree_01": {
+    "type": "oak",
+    "x": 100,
+    "y": 200,
+    "collidable": true
+  },
+  "rock_01": {
+    "type": "boulder",
+    "x": 300,
+    "y": 150,
+    "collidable": true
+  },
+  "spawn_point": {
+    "type": "portal",
+    "x": 0,
+    "y": 0,
+    "active": false
+  }
+}
+*/
 
 // Dedicated to God the Father
 // All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
